adb657463a23f59ac82a0b48238be02967d0a75f
pavithramk18 <pavithram.kumaresan@gmail.com>
2023-08-07 19:09:50 +0530

    Sample

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-console.log("List of Inbuilt Date Methods in Javascript")
+console.log("List of Inbuilt Date Methods in Javascript");
+
+const date= new Date();  //Creates the date object with the keyword new to see todays date 
+console.log(date.toDateString()); //Mon Aug 07 2023
