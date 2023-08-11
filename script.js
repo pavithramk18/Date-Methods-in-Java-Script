dddbaa23ed0a2112fde540007ebf8d436b9cb0e9
pavithramk18 <pavithram.kumaresan@gmail.com>
2023-08-11 17:48:39 +0530

    Date methods

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ console.log("List of Inbuilt Date Methods in Javascript");
 
 const date= new Date();  //Creates the date object with the keyword new to see todays date 
 console.log(date.toDateString()); //Mon Aug 07 2023
+
+//toDateString==>Returns only the date potion without time and any elements of the object
+
+const d= new Date();
+console.log(d.toDateString());  //  Fri Aug 11 2023
